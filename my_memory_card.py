292dbecff06ac44c074a6edfc2e124a5402d140e
butilkaketchupa228 <149230294+butilkaketchupa228@users.noreply.github.com>
2023-10-28 13:34:48 +0600

    Add files via upload

---
+++ my_memory_card.py
@@ -0,0 +1,13 @@
+#создай приложение для запоминания информации
+from PyQT5.QTCore import QTCore
+from PyQT5.QWidgets import (QApplication,QWidget,QBoxLayot,QGroupBox,QRadioButton,QPushButton,QLabel)
+
+
+app = QApplication([])
+
+
+window=QWidget
+window.setwindowTitle('Memo Card')
+
+'''Интерес приложения Memory Card'''
+btn_OK=QRadioButton
